f1aad5b435aea0d934b5fb0e7af40c7e99c450f8
LEZIE TRA BI <lezie01.tra@gmail.com>
2025-12-10 16:47:31 +0000

    est paramétrée pour suivre

--- types/data-file.tsx
+++ types/data-file.tsx
@@ -4,7 +4,10 @@
 
 import { ColumnDef } from "@tanstack/react-table"
 import { ArrowUpDown } from "lucide-react"
-import { Button } from "@/components/ui/button"
+import
+
+
+{ Button } from "@/components/ui/button"
 
 export type Payment = {
     id: string
